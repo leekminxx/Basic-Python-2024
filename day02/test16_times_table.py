2461336520dd5f8f36eef9432ba5b2fc57a38d6c
leekminxx <158007500+leekminxx@users.noreply.github.com>
2024-01-31 17:32:39 +0900

    3일차 완료

--- day02/test16_times_table.py
+++ day02/test16_times_table.py
@@ -12,3 +12,18 @@ for x in range(2, 10):
         print(f'{x}x{y} = {x*y:2d}' , end= ' ')
     print() 
 
+
+
+
+
+
+# # debugging
+#     #F9(종단점 토글) , F5(디버깅시작), F10(한줄씩 실행) , F11(함수안으로 진입)
+#     #Shift + F5(디버깅종료)
+#     print('구구단 시작 !')
+#     x = 2 
+#     for x in range(2, 9+1): # 2부터 9까지 반복 
+#         print(f'{x} 단 ---- > ') 
+#         for y in range(1 , 9+1): # 1부터 9까지 반복
+#             print(f'{x}x{y} = {x*y:2d}' , end= ' ') # end = 엔터대신 공백으로 변경
+#         print() # 안쪽 for 문이 끝나면 마지막 엔터를 하나 추가
