f6e873da5ab71890878a419dfded72ad2f790f42
leekminxx <158007500+leekminxx@users.noreply.github.com>
2024-02-02 11:13:36 +0900

    update

--- day04/test25_web.py
+++ day04/test25_web.py
@@ -3,9 +3,7 @@
 
 #from urllib.request import * request 모듈안의 모든 내용 다 사용할 때 
 
-from urllib.request import  Request , urlopen # Request 클래스와 urlopen만 쓰겠다
-from urllib.request import  Request , urlopen #* 
-from urllib.request import  Request , urlopen #* 
+from urllib.request import Request,urlopen # Request 클래스와 urlopen만 쓰겠다
 
 req = Request('https://www.naver.com/') #네이버 웹주소 (url)
 res = urlopen(req) #url주소로 웹사이트 열어달라고 요청 
--- day04/test26_main.py
+++ day04/test26_main.py
@@ -4,9 +4,10 @@
 class Temp:
     pass
 
-if __name__ == ' __main__':
+if __name__ == '__main__':
     print('제가 시작점입니다.')
-    
+    tt = Temp()
+    print(type(tt))
 
 
 
